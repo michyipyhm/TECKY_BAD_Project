c9467981417a5284c898e422f3c4855d55f3dbdc
michyipyhm <micmicyip@hotmail.com>
2024-09-27 15:32:13 +0800

    added npm run dev v2

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 import express , { Request, Response }from "express";
 import dotenv from "dotenv";
 
-const app = express();
+const main = express();
 
 dotenv.config();
 
-app.get("/", function (req: Request, res: Response) {
+main.get("/", function (req: Request, res: Response) {
   res.end("Hello World");
 });
 
 const PORT = 8080;
 
-app.listen(PORT, () => {
+main.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}/`);
 });

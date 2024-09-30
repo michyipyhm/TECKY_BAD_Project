8cbbf14428f6dd39e963e5b250d15d2b201063a6
ＭＡＫＴＳＺＫＥＩ <mtszkei@gmail.com>
2024-09-30 15:42:45 +0800

    memberRouter

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import express , { Request, Response }from "express";
+import expressSession from "express-session";
 import dotenv from "dotenv";
+import { memberRouter } from "./routes/memberRouter";
 
 const main = express();
 
@@ -8,6 +10,8 @@ dotenv.config();
 main.get("/", function (req: Request, res: Response) {
   res.end("Hello World");
 });
+
+main.use(memberRouter);
 
 const PORT = 8080;
 

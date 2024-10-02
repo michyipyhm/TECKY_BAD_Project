587e0467a65c6e8c872966473e1275a3fcf00b02
ＭＡＫＴＳＺＫＥＩ <mtszkei@gmail.com>
2024-10-02 11:16:31 +0800

    knex seeds

--- routes/memberRouter.ts
+++ routes/memberRouter.ts
@@ -1,36 +1,12 @@
 import express, { Request, Response } from "express";
 import formidable from 'formidable';
-
-import Knex from "knex";
-const knexConfigs = require("../knexfile");
-const configMode = process.env.NODE_ENV || "development";
-const knexConfig = knexConfigs[configMode];
-const knex = Knex(knexConfig);　//knex instance
+import { knex } from 'knex';
 
 export const memberRouter = express.Router();
 
+memberRouter.post("/register", registerNewMember);
 
-async function register(){
-
-    // const reg = await knex.insert([
-    //         {
-    //             username: "admin", // username,
-    //             password: "123123", // password,
-    //             phone: "99887766", // phone,
-    //             address: "HongKong", //address,
-    //             email: "admin@admin.com", // email,
-    //             admin: true
-    //         }
-    //     ]).into('members').returning('id')
-
-        const result = await knex.select('username', 'email').from('members').where('username', 'admin')
-        // const username = result.username
-    // console.log(reg)
-    console.log(result.length)
-}
-register()
-
-memberRouter.post("/register", async (req: Request, res: Response) => {
+async function registerNewMember(req: Request, res: Response){
 
     const data = req.body;
     const username = data.username;
@@ -50,8 +26,7 @@ memberRouter.post("/register", async (req: Request, res: Response) => {
         return;
       }
 
-
-    await knex.insert([
+    const insertResult = await knex.insert([
         {
             username: username,
             password: password,
@@ -60,7 +35,11 @@ memberRouter.post("/register", async (req: Request, res: Response) => {
             email: email,
             admin: false
         }
-    ]).into('members')
+    ]).into('members').returning('id')
+    req.session.userId = insertResult[0].id
+    res.json({ message: "Register successful" });
+}
+
 
     // const sql_1 = `Select * from member where username = '${username}'`;
     // const userResult = await pgClient.query(sql_1);
@@ -76,4 +55,3 @@ memberRouter.post("/register", async (req: Request, res: Response) => {
     // const insertResult = await pgClient.query(sql);
     // req.session.userId = insertResult.rows[0].id
     // res.json({ message: "Register successful" });
-  });

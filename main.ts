d9dfec415a7a22f86ff9c829e855614dc1ff2208
VictorYsc <victorysc0617@gmail.com>
2024-10-07 10:25:50 +0800

    update products page

--- main.ts
+++ main.ts
@@ -2,12 +2,11 @@ import express, { Request, Response } from "express";
 import expressSession from "express-session";
 import dotenv from "dotenv";
 import { userRouter } from "./routes/userRouter";
-// import { filter } from "./routes/filterRoutes";
-// import { productRoutes } from "./routes/productRoutes";
+
 import Knex from "knex";
 import { replicateAi } from "./routes/replicateAI";
-import { filter } from './routes/filterRoutes';
-import { productRoutes } from "./routes/productRoutes";
+import { productsRoutes } from './routes/productsRoutes';
+import { productDetailsRoutes } from "./routes/productDetailsRoutes";
 
 dotenv.config();
 
@@ -39,7 +38,8 @@ main.get("/", function (req: Request, res: Response) {
 
 main.use(userRouter);
 main.use(replicateAi);
-
+main.use(productsRoutes);
+main.use(productDetailsRoutes);
 const PORT = 8080;
 
 main.listen(PORT, () => {
---
+++ routes/productDetailsRoutes.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response } from "express";
+// import formidable from "formidable";
+import { knex } from "../main";
+// import { createTypeReferenceDirectiveResolutionCache } from "typescript";
+export const productDetailsRoutes = express.Router();
+productDetailsRoutes.post("/productDetails", productdetails);
+
+async function productdetails(req: Request, res: Response) {
+ 
+  try {
+    
+    const product_info_result = await knex
+      .select(
+        "product_image.image_path",
+        "category_name as product_type",
+        "category_type",
+        "model.name as model_name",
+        "color.name as color_name",
+        "products.product_name",
+        "products.product_price",
+        "products.product_quantity",
+        "products.id as product_id"
+     
+      )
+      .from("product_option As po")
+      .join("products", "products.id", "po.products_id")
+      .join("color", "color.id", "po.color_id")
+      .join("model", "model.id", "po.model_id")
+      .join("category", "category.id", "category_id")
+      .join("product_image", "products.id", "product_image.product_id")
+      .where("products.id", req.body.product_id)
+    
+    res.json(
+      product_info_result.map((row: any) => ({
+        id: row.product_id,
+        product_name: row.product_name,
+        product_price: row.product_price,
+        image_path: row.image_path,
+        product_id: row.product_id,
+        product_quantity: row.product_quantity,
+      }))
+    );
+  } catch (error) {
+    res.status(500).json({
+      message: "An error occurred while retrieving the product information.",
+    });
+  }
+}

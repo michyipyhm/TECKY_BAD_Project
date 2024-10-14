f443b086b3baa2c06b3ded5ba0f645448867a275
michyipyhm <micmicyip@hotmail.com>
2024-10-14 10:38:43 +0800

    fix profile

--- public/js/index.js
+++ public/js/index.js
@@ -1,122 +1,123 @@
 window.onload = async () => {
-  // await getUserProfile();
+  await getUserProfile();
   await replicateAi();
-  // chatBoxToggle();
-  // chatBox();
+  chatBoxToggle();
 
-  // async function getUserProfile() {
-  //   const res = await fetch("/userinfo");
-  //   const data = await res.json();
-  //   const userInfo = data.userInfo;
-  //   const userArea = document.querySelector(".userArea");
+  async function getUserProfile() {
+    const res = await fetch("/userinfo");
+    const data = await res.json();
+    const userInfo = data.userInfo;
+    const userArea = document.querySelector(".userArea");
 
-  //   if (res.ok && userInfo) {
-  //     userArea.innerHTML = `
-  //               <div class="userInfo"><span class="userInfoFont">${userInfo.username}</span></div>
-  //               <div class="settingBtn"><a href="/profile.html"><span class="userInfoFont">Profile</span></a></div>
-  //               <div class="logout"><button type="button" id="logoutBtn" class="userInfoFont">Logout</button></div>
-  //           `;
-  //     const logoutEle = document.querySelector("#logoutBtn");
-  //     if (logoutEle) {
-  //       logoutEle.addEventListener("click", async () => {
-  //         const res = await fetch("/logout", { method: "POST" });
-  //         if (res.ok) {
-  //           window.location.reload();
-  //         } else {
-  //           alert("Logout failed");
-  //         }
-  //       });
-  //     }
-  //   } else {
-  //     bindAuthButtons();
-  //   }
-  // }
+    console.log("User Info:", userInfo)
 
-  // function bindAuthButtons() {
-  //   const signupBtn = document.getElementById("signupBtn");
-  //   const loginBtn = document.getElementById("loginBtn");
-  //   const signUpOverlay = document.getElementById("signUpOverlay");
-  //   const loginOverlay = document.getElementById("loginOverlay");
-  //   const signUpCloseBtn = document.getElementById("signUpCloseBtn");
-  //   const loginCloseBtn = document.getElementById("loginCloseBtn");
+    if (res.ok && userInfo) {
+      userArea.innerHTML = `
+                <div class="userInfo"><span class="userInfoFont">${userInfo.username}</span></div>
+                <div class="settingBtn"><a href="/profile.html"><span class="userInfoFont">Profile</span></a></div>
+                <div class="logout"><button type="button" id="logoutBtn" class="userInfoFont">Logout</button></div>
+            `;
+      const logoutEle = document.querySelector("#logoutBtn");
+      if (logoutEle) {
+        logoutEle.addEventListener("click", async () => {
+          const res = await fetch("/logout", { method: "POST" });
+          if (res.ok) {
+            window.location.reload();
+          } else {
+            alert("Logout failed");
+          }
+        });
+      }
+    } else {
+      bindAuthButtons();
+    }
+  }
 
-  //   if (signupBtn) {
-  //     signupBtn.onclick = function () {
-  //       signUpOverlay.style.display = "flex";
-  //     };
-  //   }
+  function bindAuthButtons() {
+    const signupBtn = document.getElementById("signupBtn");
+    const loginBtn = document.getElementById("loginBtn");
+    const signUpOverlay = document.getElementById("signUpOverlay");
+    const loginOverlay = document.getElementById("loginOverlay");
+    const signUpCloseBtn = document.getElementById("signUpCloseBtn");
+    const loginCloseBtn = document.getElementById("loginCloseBtn");
 
-  //   if (loginBtn) {
-  //     loginBtn.onclick = function () {
-  //       loginOverlay.style.display = "flex";
-  //     };
-  //   }
+    if (signupBtn) {
+      signupBtn.onclick = function () {
+        signUpOverlay.style.display = "flex";
+      };
+    }
 
-  //   if (signUpCloseBtn) {
-  //     signUpCloseBtn.onclick = function () {
-  //       signUpOverlay.style.display = "none";
-  //     };
-  //   }
+    if (loginBtn) {
+      loginBtn.onclick = function () {
+        loginOverlay.style.display = "flex";
+      };
+    }
 
-  //   if (loginCloseBtn) {
-  //     loginCloseBtn.onclick = function () {
-  //       loginOverlay.style.display = "none";
-  //     };
-  //   }
+    if (signUpCloseBtn) {
+      signUpCloseBtn.onclick = function () {
+        signUpOverlay.style.display = "none";
+      };
+    }
 
-  //   const registerForm = document.querySelector("#registerForm");
-  //   if (registerForm) {
-  //     registerForm.addEventListener("submit", async (e) => {
-  //       e.preventDefault();
-  //       console.log("ReG");
-  //       const username = e.target.registerUsername.value;
-  //       const password = e.target.registerPassword.value;
-  //       const email = e.target.registerEmail.value;
+    if (loginCloseBtn) {
+      loginCloseBtn.onclick = function () {
+        loginOverlay.style.display = "none";
+      };
+    }
 
-  //       const body = { username, password, email };
-  //       const res = await fetch("/register", {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(body),
-  //       });
+    const registerForm = document.querySelector("#registerForm");
+    if (registerForm) {
+      registerForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        console.log("ReG");
+        const username = e.target.registerUsername.value;
+        const password = e.target.registerPassword.value;
+        const email = e.target.registerEmail.value;
 
-  //       if (res.ok) {
-  //         window.location.reload();
-  //       } else {
-  //         const data = await res.json();
-  //         alert(data.message);
-  //       }
-  //     });
-  //   }
+        const body = { username, password, email };
+        const res = await fetch("/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        });
 
-  //   const loginForm = document.querySelector("#loginForm");
-  //   if (loginForm) {
-  //     loginForm.addEventListener("submit", async (e) => {
-  //       e.preventDefault();
+        if (res.ok) {
+          window.location.reload();
+        } else {
+          const data = await res.json();
+          alert(data.message);
+        }
+      });
+    }
 
-  //       const email = e.target.loginEmail.value;
-  //       const password = e.target.loginPassword.value;
+    const loginForm = document.querySelector("#loginForm");
+    if (loginForm) {
+      loginForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-  //       const body = { email, password };
-  //       const res = await fetch("/login", {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(body),
-  //       });
+        const email = e.target.loginEmail.value;
+        const password = e.target.loginPassword.value;
 
-  //       if (res.ok) {
-  //         window.location.reload();
-  //       } else {
-  //         const data = await res.json();
-  //         alert(data.message);
-  //       }
-  //     });
-  //   }
-  // }
+        const body = { email, password };
+        const res = await fetch("/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        });
+
+        if (res.ok) {
+          window.location.reload();
+        } else {
+          const data = await res.json();
+          alert(data.message);
+        }
+      });
+    }
+  }
 
   async function replicateAi() {
     const promptForm = document.querySelector("#promptForm");
